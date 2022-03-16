6669438d39fff9b50d6f0a5cd983c3445db9617f
Charles M <charle.monot@gmail.com>
2022-03-16 10:29:16 +0100

    latest version Movies API

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,21 @@ var request = require('sync-request')
 
 var movieModel = require('../models/movies')
 
-var myApiKey = "8087f408100c3de021fda9e650563612"
+var myApiKey = "92432c52e988a085299d019efe9dbeda"
 
 router.get('/new-movies', function(req, res, next) {
-  var data = request('GET',`https://api.themoviedb.org/3/discover/movie?api_key=${myApiKey}&language=fr-FR&region=FR&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&release_date.lte=2020-01-01`)
+  var data = request('GET',`https://api.themoviedb.org/3/discover/movie?api_key=${myApiKey}&language=fr-FR&region=FR&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&release_date.lte=2022-01-01`)
   var dataParse = JSON.parse(data.body)
-  
   res.json({result: true, movies:dataParse.results})
 });
 
 router.post('/wishlist-movie', async function(req, res, next) {
-
+  console.log(req.body);
   var newMovie = new movieModel({
-    movieName: req.body.name,
-    movieImg: req.body.img
+    movieName: req.body.movieName,
+    movieImg: req.body.movieImg
   })
-
+  
   var movieSave = await newMovie.save()
 
   var result = false
@@ -32,7 +31,7 @@ router.post('/wishlist-movie', async function(req, res, next) {
 });
 
 router.delete('/wishlist-movie/:name', async function(req, res, next) {
-
+  console.log(req.params.name);
   var returnDb = await movieModel.deleteOne({ movieName: req.params.name})
 
   var result = false
